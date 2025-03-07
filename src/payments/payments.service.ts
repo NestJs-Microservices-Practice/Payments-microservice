c787a1ef3ac99ab76408949120f03cc99814f1da
itsFDavid <FDavid04@icloud.com>
2025-03-06 18:18:34 -0600

    feat: integrate NATS transport in Payments module and service for payment event handling

--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -1,17 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
 import { envs } from 'src/config/envs';
 import Stripe from 'stripe';
 import { PaymentSessionDto } from './dto/payment-session.dto';
 import { Request, Response } from 'express';
+import { SERVICES } from 'src/config';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
 export class PaymentsService {
   private readonly stripe = new Stripe(envs.stripeSecret);
+  private readonly logger = new Logger(PaymentsService.name);
+
+  constructor(
+    @Inject(SERVICES.NATS_SERVICE) private readonly client: ClientProxy,
+  ) {}
 
   async createPaymentSession(paymentSessionDto: PaymentSessionDto) {
-    const { currency, items, orderId} = paymentSessionDto;
-    
-    const lineItems = items.map( items => {
+    const { currency, items, orderId } = paymentSessionDto;
+
+    const lineItems = items.map((items) => {
       return {
         price_data: {
           currency: currency,
@@ -21,21 +28,20 @@ export class PaymentsService {
           unit_amount: Math.round(items.price * 100), // <- Esto seria como si fuese 20 dolares, ya que stripe trabaja con centavos
         },
         quantity: items.quantity,
-      }
-    })
+      };
+    });
 
     const session = this.stripe.checkout.sessions.create({
       // Colocar el id de la orden
       payment_intent_data: {
         metadata: {
-          order_id: orderId
-        }
+          order_id: orderId,
+        },
       },
       line_items: lineItems,
       mode: 'payment',
       success_url: envs.successUrl,
       cancel_url: envs.cancelUrl,
-
     });
     return session;
   }
@@ -50,7 +56,7 @@ export class PaymentsService {
       event = this.stripe.webhooks.constructEvent(
         req['rawBody'],
         sig,
-        endpointSecret
+        endpointSecret,
       );
     } catch (error) {
       res.status(400).send(`Webhook Error: ${error.message}`);
@@ -59,12 +65,14 @@ export class PaymentsService {
 
     switch (event.type) {
       case 'charge.succeeded':
-          const chargeSucceeded = event.data.object;
-          // TODO: Llamar nuetsro microservicio de ordenes
-          console.log({
-            metadata: chargeSucceeded.metadata,
-            orderId: chargeSucceeded.metadata.order_id,
-          });
+        const chargeSucceeded = event.data.object;
+        const payload = {
+          stripePaymentId: chargeSucceeded.id,
+          orderId: chargeSucceeded.metadata.order_id,
+          receiptUrl: chargeSucceeded.receipt_url,
+        };
+        this.client.emit('payment.succeeded', payload);
+        // this.logger.log({ payload });
         break;
       default:
         console.log(`Unhandled event type ${event.type}`);
@@ -72,8 +80,7 @@ export class PaymentsService {
     }
 
     return res.status(200).json({
-      sig
+      sig,
     });
   }
-
 }
